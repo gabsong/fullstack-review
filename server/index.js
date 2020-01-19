6877384c14888b86e6eeaa262f216067fa5b87ca
Gabriel Song <gabsong4@gmail.com>
2020-01-18 16:07:49 -0800

    Revert error handling code

--- server/index.js
+++ server/index.js
@@ -9,23 +9,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-app.post('/repos', function (req, res) {
+app.post('/repos', function(req, res) {
   github.getReposByUsername(req.body.data, (error, body) => {
     if (error) {
       console.log('Error in getReposByUsername', error);
     } else {
-      mongo.save(body, (error, success) => {
+      mongo.save(body, (error, body, next) => {
         if (error) {
-          // handle "existing primary key" error
+          // error handling for duplicate primary keys
+          // http://thecodebarbarian.com/mongoose-error-handling.html
+          if (error.name === 'MongoError' && error.code === 11000) {
+            // next(new Error('There was a duplicate key error'));
+          } else {
+            // next();
+          }
         } else {
-          res.status(201).send(`Repos from ${req.body.data} added to the database`);
+          res.status(201).send(`Repos from ${body} added to the database`);
         }
       });
     }
   });
 });
 
-app.get('/repos', function (req, res) {
+app.get('/repos', function(req, res) {
   mongo.find((error, results) => {
     if (error) {
       console.log('Error querying mongo:', error);
