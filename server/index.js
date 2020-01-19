451e165be2c4338d4b05e5bb4ecc0a1e484bf2cc
Gabriel Song <gabsong4@gmail.com>
2020-01-18 17:00:27 -0800

    Demo with Lily

--- server/index.js
+++ server/index.js
@@ -9,20 +9,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-app.post('/repos', function(req, res) {
+app.post('/repos', function(req, res, next) {
   github.getReposByUsername(req.body.data, (error, body) => {
     if (error) {
       console.log('Error in getReposByUsername:', error);
     } else {
-      mongo.save(body, (error, body, next) => {
+      mongo.save(body, (error, body) => {
         if (error) {
           // error handling for duplicate primary keys
-          // http://thecodebarbarian.com/mongoose-error-handling.html
           if (error.name === 'MongoError' && error.code === 11000) {
             console.log('Duplicate primary key:', error);
-            // next(new Error('There was a duplicate key error'));
+            res.status(200).send(error);
           } else {
-            // next();
+            res.status(400).send(error);
           }
         } else {
           res.status(201).send(`Repos from ${body} added to the database`);
@@ -32,10 +31,11 @@ app.post('/repos', function(req, res) {
   });
 });
 
-app.get('/repos', function(req, res) {
+app.get('/repos', function(req, res, next) {
   mongo.find((error, results) => {
     if (error) {
       console.log('Error querying mongo:', error);
+      res.status(404).send(error);
     } else {
       res.status(200).send(results);
     }
