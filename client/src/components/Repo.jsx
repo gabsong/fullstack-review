24206888842c9782f676ef7338ae53a9d5f99824
Gabriel Song <gabsong4@gmail.com>
2020-01-18 17:14:41 -0800

    Add star count next to repo name

--- client/src/components/Repo.jsx
+++ client/src/components/Repo.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Repo = (props) => (
   <div>
     <div className="repo-name">
-      <a href={props.repo.repo_url}>{props.repo.repo_name}</a>
+      <div>
+        <a href={props.repo.repo_url}>{props.repo.repo_name}</a>
+        <span> {String.fromCodePoint(0x2B50)}{props.repo.stargazers_count}</span>
+      </div>
     </div>
   </div>
 )
